1eb7c95e31d6138c469c7ec2190e9e3f490aeeb7
Steve Nay <steven.nay@gmail.com>
2011-07-14 16:08:59 -0600

    Feature-complete compared to Ruby version

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 from bottle import route, run, debug
 
+# Default values for the state
 thermostat = 75
+stereo_volume = 0
 
+# Set the temperature of the thermostat
 @route('/thermostat/:temp')
-def adjust_temp(temp):
+def get_temp(temp):
     temp = int(temp)
     global thermostat
     if temp >= 65 and temp <= 85:
@@ -11,6 +14,17 @@ def adjust_temp(temp):
         thermostat = temp
     return '{"temperature": %s}' % thermostat
 
+# Set the volume of the stereo
+@route('/stereo_volume/:level')
+def set_volume(level):
+    level = int(level)
+    global stereo_volume
+    if level >= 0 and level <= 25:
+        print "Stereo level now %s. Was %s." % (level, stereo_volume)
+        stereo_volume = level
+    return '{"stereo_volume": %s}' % stereo_volume
+
+# Run the server
 debug(True)
 run(host='localhost', port=8080, reloader=True)
 
